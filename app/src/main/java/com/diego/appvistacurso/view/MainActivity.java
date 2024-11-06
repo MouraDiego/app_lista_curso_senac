9275f0de367654c111e6c1c62ad73b3371fd44be
MouraDiego <diegoaamoura@gmail.com>
2024-11-05 21:43:17 -0300

    Criado método salvar e limpar no Java.

--- app/src/main/java/com/diego/appvistacurso/view/MainActivity.java
+++ app/src/main/java/com/diego/appvistacurso/view/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.diego.appvistacurso.R;
@@ -17,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
     String priPessoa;
     String segPessoa;
 
+    EditText primeiroNome, sobroNome, curSo, teleContato;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         pessoa = new Pessoa();
 
@@ -29,12 +35,14 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setNomeCurso("Desenvolvimento de Sistemas");
         pessoa.setTelefoneContato("34-99157-9741");
 
+/*
         outraPessoa = new Pessoa();
 
         outraPessoa.setPrimeiroNome("Tiago");
         outraPessoa.setSobreNome("Henrique de Araujo Moura");
         outraPessoa.setNomeCurso("Educação Fisica");
         outraPessoa.setTelefoneContato("34-99157-9637");
+*/
 
 /* Get usado para pegar as informações do set
         priPessoa = "Nome: " + pessoa.getPrimeiroNome() + "\nSobrenome: " +
@@ -48,12 +56,34 @@ public class MainActivity extends AppCompatActivity {
                 outraPessoa.getTelefoneContato();
 
 */
-        //Criado log para capturar informações
+
+/*        //Criado log para capturar informações
         Log.i("Informações Pessoais", pessoa.toString());
         Log.i("Informa Pessoais", outraPessoa.toString());
+*/
+        primeiroNome = findViewById(R.id.edit_primeiro_nome);
+        sobroNome = findViewById(R.id.edit_sobrenome);
+        curSo = findViewById(R.id.edit_curso_desejado);
+        teleContato = findViewById(R.id.edit_telefone_contato);
 
-        int parar = 0;
+        primeiroNome.setText(pessoa.getPrimeiroNome());
+        sobroNome.setText(pessoa.getSobreNome());
+        curSo.setText(pessoa.getNomeCurso());
+        teleContato.setText(pessoa.getTelefoneContato());
 
+        Button limpar = findViewById(R.id.btn_limpar);
+        limpar.setOnClickListener(view -> limparResultado());
+
+        Button finalizar = findViewById(R.id.btn_finalizar);
+        finalizar.setOnClickListener(view -> finish());
+
+    }
+
+    private void limparResultado() {
+        primeiroNome.setText(" ");
+        sobroNome.setText(" ");
+        curSo.setText(" ");
+        teleContato.setText(" ");
     }
 
 }
