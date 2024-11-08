20825071fdc40066454f80372c22be2ad61626f3
MouraDiego <diegoaamoura@gmail.com>
2024-11-07 21:19:59 -0300

    Botões salvar, finalizar e limpar funcionando.

--- app/src/main/java/com/diego/appvistacurso/view/MainActivity.java
+++ app/src/main/java/com/diego/appvistacurso/view/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.diego.appvistacurso.R;
 import com.diego.appvistacurso.model.Pessoa;
@@ -20,7 +21,15 @@ public class MainActivity extends AppCompatActivity {
     String priPessoa;
     String segPessoa;
 
-    EditText primeiroNome, sobroNome, curSo, teleContato;
+    EditText edit_primeiro_nome;
+    EditText edit_sobrenome;
+    EditText edit_curso_desejado;
+    EditText edit_telefone_contato;
+
+    Button btn_limpar;
+    Button btn_finalizar;
+    Button btn_salvar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,29 +70,51 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Informações Pessoais", pessoa.toString());
         Log.i("Informa Pessoais", outraPessoa.toString());
 */
-        primeiroNome = findViewById(R.id.edit_primeiro_nome);
-        sobroNome = findViewById(R.id.edit_sobrenome);
-        curSo = findViewById(R.id.edit_curso_desejado);
-        teleContato = findViewById(R.id.edit_telefone_contato);
+        edit_primeiro_nome = findViewById(R.id.edit_primeiro_nome);
+        edit_sobrenome = findViewById(R.id.edit_sobrenome);
+        edit_curso_desejado = findViewById(R.id.edit_curso_desejado);
+        edit_telefone_contato = findViewById(R.id.edit_telefone_contato);
 
-        primeiroNome.setText(pessoa.getPrimeiroNome());
-        sobroNome.setText(pessoa.getSobreNome());
-        curSo.setText(pessoa.getNomeCurso());
-        teleContato.setText(pessoa.getTelefoneContato());
+        edit_primeiro_nome.setText(pessoa.getPrimeiroNome());
+        edit_sobrenome.setText(pessoa.getSobreNome());
+        edit_curso_desejado.setText(pessoa.getNomeCurso());
+        edit_telefone_contato.setText(pessoa.getTelefoneContato());
 
-        Button limpar = findViewById(R.id.btn_limpar);
-        limpar.setOnClickListener(view -> limparResultado());
+        btn_limpar = findViewById(R.id.btn_limpar);
+        btn_finalizar = findViewById(R.id.btn_finalizar);
+        btn_salvar = findViewById(R.id.btn_salvar);
 
-        Button finalizar = findViewById(R.id.btn_finalizar);
-        finalizar.setOnClickListener(view -> finish());
+        btn_limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit_primeiro_nome.setText(" ");
+                edit_sobrenome.setText(" ");
+                edit_curso_desejado.setText(" ");
+                edit_telefone_contato.setText(" ");
+            }
+        });
 
-    }
+        btn_finalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte Sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
 
-    private void limparResultado() {
-        primeiroNome.setText(" ");
-        sobroNome.setText(" ");
-        curSo.setText(" ");
-        teleContato.setText(" ");
+        btn_salvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoa.setPrimeiroNome(edit_primeiro_nome.getText().toString());
+                pessoa.setSobreNome(edit_sobrenome.getText().toString());
+                pessoa.setNomeCurso(edit_curso_desejado.getText().toString());
+                pessoa.setTelefoneContato(edit_telefone_contato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Dados Salvos" + pessoa.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 }
